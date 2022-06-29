227508916f46892d6d79dbd0c720c5efd5e8e8d2
masaya-T <tapgjtwmdkpl@gmail.com>
2022-06-29 22:58:54 +0900

    add music player

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -2,34 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-const Player = () => (
-  <AudioPlayer
-    autoPlay
-    src="http://example.com/audio.mp3"
-    onPlay={e => console.log("onPlay")}
-    // other props here
-  />
-);
+import React from 'react';
+import './index.css';
 
-function App() {
+function Player(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AudioPlayer
+      autoPlay
+      src={props.value}
+      onPlay={e => console.log("onPlay")}
+    // other props here
+    />
   );
+}
+
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            Hello World
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <footer>
+          <Player
+            value={"path"}
+          />
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
